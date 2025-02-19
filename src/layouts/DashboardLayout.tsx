cd1c1a144742abd5a12182332c7dd41549fbe147
Urkel <abmichael109@gmail.com>
2025-02-19 15:52:22 +0100

    Made some changes

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -5,13 +5,12 @@ import { Outlet } from 'react-router-dom'
 
 const DashboardLayout: React.FC = () => {
   return (
-    <div className='flex'>
+    <div className='flex min-h-[100vh]'>
         <Sidebar />
-        <div className='shrink-0 w-full md:w-[700px] p-5 relative'>
+        <div className='shrink-0 w-full h-ful  md:w-[700px] p-5 relative'>
             <Outlet />
         </div>
         <NetworkUssdCodes />
-        
     </div>
   )
 }
--- src/layouts/TransactionLayout.tsx
+++ src/layouts/TransactionLayout.tsx
@@ -5,7 +5,7 @@ import { Outlet } from 'react-router-dom'
 const TransactionLayout: React.FC = () => {
   return (
     <div className=' h-full'>
-        <div className='bg-transparent backdrop-blur-xl h-full'>
+        <div className='bg-transparent backdrop-blur-xl'>
         <Outlet />
         </div>
         <TransactionSummary />
